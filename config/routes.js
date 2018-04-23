8b329f0e28d093d1fd3bb960288779e41e8ce6c3
Cindy Schlegel <invertsor@gmail.com>
2018-04-23 15:29:19 -0700

    login/registration Cindy

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,14 @@
 
 const index = require("../controllers/index.js")
+const user = require("../controllers/user.js")
+
 module.exports = function(app){
 
   app.get('/', index.home);
+
+  app.post('/registration', user.register);
+
+  app.post('/login', user.login);
+
 
 }
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,52 @@
 const knex = require("../db/knex.js");
+const hasher = require("../db/hasher.js");
 
 module.exports = {
- home: function(req, res) {
-   res.render("index");
+  home: function(req, res) {
+    knex('user').then((results)=>{
+       if(req.session.errors){
+       req.session.errors = [];
+     }
+       res.render('index', {users: results, errors: req.session.errors});
+     })
+   },
+
+   login: (req,res)=>{
+     knex('user')
+       .where('email', req.body.email)
+       .then((result)=>{
+         let user = result[0];
+         hasher.compare(user,req.body).then((isMatch)=>{
+
+         })
+         if(isMatch){
+           req.session.user_id = user.id;
+           req.session.save(()=>{
+             if(user.user_type === "charity"){
+             res.redirect('/charity_dashboard');
+
+         }else if
+           (user.user_type === "donor"){
+           res.redirect('/donor_dashboard');
+         }
+       }).catch(()=>{
+         res.redirect('/index');
+       })
+     })
+   }
  },
-}
+ register: (req, res)=> {
+  hasher.hash(req.body).then((user)=>{
+  knex('user').insert({
+    name:user.name,
+    email:user.email,
+    password:user.password
+  }).then(()=>{
+    res.redirect('/index');
+  }).catch(()=>{
+    req.session.errors.push('Registration Invalid')
+  })
+})
+},
+
+ }
